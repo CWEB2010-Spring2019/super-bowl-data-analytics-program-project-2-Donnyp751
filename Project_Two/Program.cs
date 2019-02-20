80e6c0a8b3ceed01dd7d888ec3c813e4f77145cb
Donald Posterick <donny.p@live.com>
2019-02-20 12:18:07 -0600

    Added superBowlEntry objects

--- Project_Two/Program.cs
+++ Project_Two/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 
 namespace Project_Two
 {
@@ -10,7 +13,27 @@ namespace Project_Two
             * or guide them through generating the file.
             **/
 
-           
+            string fileName = @"..\..\..\..\Super_Bowl_Project.csv";
+
+            List<string> rawData = File.ReadAllText(fileName).Split('\n', StringSplitOptions.RemoveEmptyEntries).ToList();
+            List<SuperBowlEntry> entries = new List<SuperBowlEntry>();
+            foreach (string entry in rawData)
+            {
+                try
+                {
+                    entries.Add(new SuperBowlEntry(entry));
+                }
+                catch
+                {
+                    //eat exceptions from entry that failed to load.
+                }
+            }
+            entries.ForEach(entry => Console.WriteLine(entry.ToString()));
+            
+            Console.ReadKey();
+
+
+
         }
     }
 }
---
+++ Project_Two/SuperBowlEntry.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Project_Two
+{
+    class SuperBowlEntry
+    {
+        private static List<string> monthsLutList = new List<string>()
+        {
+            "Jan",
+            "Feb"
+        };
+        public string WinningTeam { get; set; }
+        public string WinningCoach { get; set; }
+        public string WinningQB { get; set; }
+        public DateTime DateTime { get; set; }
+        public string SuperbowlNumerial { get; set; }
+        public int Attendance { get; set; }
+        public int WinningPoints { get; set; }
+        public string LosingTeam { get; set; }
+        public string LosingCoach { get; set; }
+        public string LosingQB { get; set; }
+        public int LosingPoints { get; set; }
+        public string MVP { get; set; }
+        public string Stadium { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        private string originalEntry;
+
+        public SuperBowlEntry(string entryText)
+        {
+            this.originalEntry = entryText;
+            string[] fields = entryText.Split(',');
+            if (fields.Length != 15)
+            {
+                throw new Exception("INVALID ENTRY");
+            }
+            this.DateTime = convertToDateTime(fields[0]);
+            this.SuperbowlNumerial = fields[1];
+            this.Attendance = Convert.ToInt32(fields[2]);
+            this.WinningQB = fields[3];
+            this.WinningCoach = fields[4];
+            this.WinningTeam = fields[5];
+            this.WinningPoints = Convert.ToInt32(fields[6]);
+            this.LosingQB = fields[7];
+            this.LosingCoach = fields[8];
+            this.LosingTeam = fields[9];
+            this.LosingPoints = Convert.ToInt32(fields[10]);
+            this.MVP = fields[11];
+            this.Stadium = fields[12];
+            this.City = fields[13];
+            this.State = fields[14];
+
+        }
+
+        private DateTime convertToDateTime(string date)
+        {
+            string[] parts = date.Split('-');
+            if (Convert.ToInt32(parts[2]) == 0)
+            {
+                throw new Exception("INVALID ENTRY");
+            }
+            return new DateTime(Convert.ToInt32(parts[2]), monthsLutList.IndexOf(parts[1])+1, Convert.ToInt32(parts[0]));
+        }
+
+        public override string ToString()
+        {
+            return this.originalEntry;
+        }
+    }
+
+}
+
